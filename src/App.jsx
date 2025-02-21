d4d394a6a5145c5c8bc17ffe8e4093ada2240f06
Talha Khalid <hafizmtalhakhalid@gmail.com>
2025-02-21 05:57:06 +0500

    404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Signin from "./components/pages/login";
 import Home from "./components/pages/Home";
 import Signup from "./components/pages/signup";
@@ -13,6 +18,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/sign-in" element={<Signin />} />
             <Route path="/sign-up" element={<Signup />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
           </Routes>
         </Router>
       </div>
